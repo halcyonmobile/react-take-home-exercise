a7e6fe25424dc08b80fc0b89ef74f3daac9f92be
hedenica <hedenica.morais@lovelystay.com>
2025-03-17 17:22:27 -0300

    refactor(task manager): fix bugs + implement persistent tasks + ui improvements

--- src/components/TaskManager.tsx
+++ src/components/TaskManager.tsx
@@ -1,84 +1,54 @@
-import React, { useState } from "react";
-
+import { useState } from "react";
 import TaskItem from "./TaskItem";
+import TaskForm from "./TaskForm";
+import TaskFilters from "./TaskFilters";
+import { Filters, Task } from "@/models/TaskManager";
+import { useTasks } from "@/services/stores/useTasks";
 
-const TaskManager = () => {
-  const [tasks, setTasks] = useState<any[]>([
-    { id: 1, title: "Buy groceries", completed: false },
-    { id: 2, title: "Clean the house", completed: true },
-  ]);
-  const [filter, setFilter] = useState("all");
-  const [newTask, setNewTask] = useState<string>();
+interface TaskManagerProps {
+  darkMode: boolean;
+}
 
-  // Intentional bug: The filter conditions are reversed.
+const TaskManager: React.FC<TaskManagerProps> = ({ darkMode }) => {
+  const [filter, setFilter] = useState<Filters>("all");
+  const [newTask, setNewTask] = useState('');
+
+  const { tasks, setTasks } = useTasks(state => ({
+    tasks: state.tasks,
+    setTasks:  state.setTasks,
+  }));
+
   const filteredTasks = tasks.filter((task) => {
-    if (filter === "completed") return task.completed === false;
-    if (filter === "pending") return task.completed === true;
+    if (filter === "completed") return task.completed === true;
+    if (filter === "pending") return task.completed === false;
     return true;
   });
 
-  const handleAddTask = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (newTask!.trim() === "") return;
-    const newTaskObj = {
+  const handleAddTask = (title: string) => {
+    const newTaskObj: Task = {
       id: tasks.length + 1,
-      name: newTask,
+      title,
       completed: false,
     };
     setTasks([...tasks, newTaskObj]);
-    setNewTask("");
+    setFilter("all");
   };
 
-  // Intentional bug: Directly mutating the tasks array when deleting.
-  const handleDeleteTask = (id: number) => {
-    const index = tasks.findIndex((task) => task.id === id);
-    if (index !== -1) {
-      tasks.splice(index, 1);
-      setTasks(tasks);
-    }
-  };
-
-  const toggleTaskCompletion = (id: number) => {
-    const task = tasks.find((task) => task.id === id);
-
-    task.isCompleted = !task.isCompleted;
-  };
+  const getEmptyStateText = filter !== "all" 
+    ? 'No tasks found with the current filter' 
+    : 'There are no tasks at the moment';
 
   return (
-    <div className="container mx-auto bg-white p-4 rounded shadow">
-      <form onSubmit={handleAddTask} className="mb-4 flex">
-        <input
-          type="text"
-          placeholder="New task..."
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          className="flex-grow border rounded-l py-2 px-3"
-        />
-        <button type="submit" className="bg-blue-500 text-white px-4 rounded-r">
-          Add
-        </button>
-      </form>
-      <div className="flex justify-around mb-4">
-        <button onClick={() => setFilter("all")} className="text-gray-700">
-          All
-        </button>
-        <button
-          onClick={() => setFilter("completed")}
-          className="text-gray-700"
-        >
-          Completed
-        </button>
-        <button onClick={() => setFilter("pending")} className="text-gray-700">
-          Pending
-        </button>
-      </div>
+    <div className={`container min-h-52 mx-auto p-4 rounded shadow ${darkMode ? 'bg-gray-700 text-white' : 'bg-white text-black'}`}>
+      <TaskForm newTask={newTask} setNewTask={setNewTask} handleAddTask={handleAddTask} />
+      <TaskFilters filter={filter} setFilter={setFilter} darkMode={darkMode}/>
+      {filteredTasks.length === 0 && <p className="mt-8 text-center">{getEmptyStateText}</p>}
       <ul>
         {filteredTasks.map((task) => (
           <TaskItem
             key={task.id}
             task={task}
-            onDelete={handleDeleteTask}
-            onToggle={toggleTaskCompletion}
+            darkMode={darkMode}
           />
         ))}
       </ul>
