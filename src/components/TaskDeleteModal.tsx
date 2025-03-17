e9ece2add7bbe73c3de165bbb8bd20a3e502efac
hedenica <hedenica.morais@lovelystay.com>
2025-03-17 17:21:29 -0300

    feat(component): add a delete modal

---
+++ src/components/TaskDeleteModal.tsx
@@ -0,0 +1,37 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/Dialog"
+
+interface Props {
+  onDelete: () => void;
+  closeModal: () => void;
+}
+
+const TaskDeleteModal = ({ onDelete, closeModal }: Props) => {
+  return (
+      <AlertDialog open>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you sure you want to delete this task?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your task.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={closeModal}>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={onDelete}>Delete</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+  );
+};
+
+export default TaskDeleteModal;
