7cc3be3523482c802ed97067f8deefb03e931ad5
Celestino Gomes <tinorj@gmail.com>
2025-03-25 14:04:32 -0300

    Refactoring Modal Component and add tests

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { AllHTMLAttributes, DetailedHTMLProps, HTMLAttributes, ReactHTMLElement, ReactNode } from "react";
 
-type ModalProps = {
-    children?: React.ReactNode,
-    isOpen: boolean,
+interface DataProps {
+  [headerName: `data-${string}`]: string
 }
 
-const Modal = ({ children, isOpen }: ModalProps) => {
+interface ModalProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+    isOpen: boolean,
+    bgDivProps?: AllHTMLAttributes<HTMLElement> | DataProps,
+}
 
+const Modal = ({ children, isOpen, bgDivProps, ...modalDivProps }: ModalProps) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-40 backdrop-blur-sm flex justify-center items-center">
-      <div className="md:w-3/6 sm:w-5/6">
-      <div className="bg-white p-2 rounded">
-        { children }
-      </div>
+    <div role="modalBackgrond" className="fixed inset-0 bg-black bg-opacity-40 backdrop-blur-sm flex justify-center items-center" {...bgDivProps}>
+      <div role="modalSizePanel" className="md:w-3/6 sm:w-5/6">
+        <div role="modalPanel" className="bg-white p-2 rounded" {...modalDivProps}>
+          { children }
+        </div>
       </div>
     </div>
   );
---
+++ src/components/shared/__tests__/Modal.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import Modal from '../Modal';
+
+describe('Modal Component', () => {
+    it('should return null for isOpen is false', () => {
+        render(<Modal isOpen={false}>Modal</Modal>);
+
+        const bgDiv = screen.queryByRole('modalBackgrond');
+
+        expect(bgDiv).not.toBeInTheDocument();
+    });
+
+    it('should render with default values', () => {
+        render(<Modal isOpen={true}>Modal Content</Modal>);
+
+        const bgDiv = screen.getByRole('modalBackgrond');
+        const sizeDiv = screen.getByRole('modalSizePanel');
+        const mainDiv = screen.getByRole('modalPanel');
+
+        expect(screen.getByText('Modal Content')).toBeInTheDocument()
+
+        expect(bgDiv).toBeInTheDocument();
+        expect(bgDiv.className).toEqual('fixed inset-0 bg-black bg-opacity-40 backdrop-blur-sm flex justify-center items-center');
+        
+        expect(sizeDiv).toBeInTheDocument();
+        expect(sizeDiv.className).toEqual('md:w-3/6 sm:w-5/6');
+        
+        expect(mainDiv).toBeInTheDocument();
+        expect(mainDiv.className).toEqual('bg-white p-2 rounded');
+
+        screen.debug();
+    });
+});
